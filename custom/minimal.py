7de52da721ddbe83a082a522985a69312cbdae84
Andreas Stocker <xflow-systems@proton.me>
2024-06-23 14:12:46 +0200

    custom: joins working for simple example

--- custom/minimal.py
+++ custom/minimal.py
@@ -10,20 +10,29 @@ from openGraphMatching.utils import convert_graph, check_match_correctness
 # Construct the query graph
 q = nx.Graph()
 q.add_nodes_from([
-    (10, {'feat': 'User:session'}),
-    
-    (13, {'feat': 'UserFirstName:session'}),
-    (14, {'feat': 'UserFirstName:first_name'}),
-    
-    (15, {'feat': 'UserLastName:session'}),
-    (16, {'feat': 'UserLastName:last_name'}),
+    # User:session
+    (10, {'feat': 'session'}),
+    # UserFirstName:session
+    (13, {'feat': 'first_name'}),
+    # UsersLastName:session
+    (15, {'feat': 'last_name'}),
+    # # User:session
+    # (10, {'feat': 'value'}),
+    # # User:first_name
+    # (11, {'feat': 'value'}),
+    # # User:last_name
+    # (12, {'feat': 'value'}),
+    # # UserFirstName:session
+    # (13, {'feat': 'value'}),
+    # # UserFirstName:first_name
+    # (14, {'feat': 'value'}),
+    # # UsersLastName:session
+    # (15, {'feat': 'value'}),
+    # # UsersLastName:last_name
+    # (16, {'feat': 'value'}),
 ])
 
 q.add_edges_from([
-    (13, 14),
-
-    (15, 16),
-
     (10, 13),
     (10, 15),
 ])
@@ -42,16 +51,22 @@ def rev_h(h):
     return "<missing_hash>"
 
 graph_nodes = [
-    (0, {'feat': 'User:session', 'value': 'none'}),
+    # (0, {'feat': 'User:session', 'value': 'none'}),
     
-    (h("session1"), {'feat': 'UserFirstName:session', 'value': 'session1'}),
-    (h("Andy"), {'feat': 'UserFirstName:first_name', 'value': 'Andy'}),
+    # (h("session1"), {'feat': 'UserFirstName:session', 'value': 'session1'}),
+    # (h("Andy"), {'feat': 'UserFirstName:first_name', 'value': 'Andy'}),
     
-    (5, {'feat': 'UserLastName:session', 'value': 'session0'}),
-    (6, {'feat': 'UserLastName:last_name', 'value': 'Zero'}),
+    # (5, {'feat': 'UserLastName:session', 'value': 'session0'}),
+    # (6, {'feat': 'UserLastName:last_name', 'value': 'Zero'}),
 
-    (7, {'feat': 'UserFirstName:session', 'value': 'session0'}),
-    (8, {'feat': 'UserFirstName:first_name', 'value': 'Bob'}),
+    # (7, {'feat': 'UserFirstName:session', 'value': 'session0'}),
+    # (8, {'feat': 'UserFirstName:first_name', 'value': 'Bob'}),
+    # 
+    (h('session3'), {'feat': 'session'}),
+    (h('session4'), {'feat': 'session'}),
+    (h('Andy'), {'feat': 'first_name'}),
+    (h('Bob'), {'feat': 'first_name'}),
+    (h('Zero'), {'feat': 'last_name'}),
 ]
 
 graph_nodes.sort()
@@ -60,15 +75,11 @@ graph_nodes.sort()
 G = nx.Graph()
 G.add_nodes_from(graph_nodes)
 G.add_edges_from([
-    (h("session1"), h("Andy")),
-
-    (5, 6),
-
-    (7, 8),
-
-    (0, h("session1")),
-    (0, 5),
-    (0, 7),
+    (h("session3"), h("Andy")),
+    (h("session3"), h("Zero")),
+    
+    (h("session4"), h("Bob")),
+    (h("session4"), h("Zero")),
 ])
 
 print(q.nodes)
@@ -82,9 +93,9 @@ print(l[1])
 print ("\n")
 for found in l[1]:
     for key, value in found.items():
-        v = 0
+        v = ''
         for node in graph_nodes:
             if node[0] == value:
-                v = node[1]['value']
-        print(str(key) + '(' + rev_h(value) + ')' + ' = ' + str(v))
+                v = node[1]['feat']
+        print(str(key) + ' - (' + v + ') = ' + rev_h(value))
     print ("\n")
